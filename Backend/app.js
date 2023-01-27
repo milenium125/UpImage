2cc8450ded8f9c0b2089fa263f36cc9e3c231453
milenium125 <dsanmiguel582@gmail.com>
2023-01-27 11:31:57 -0500

    Actualizacion

---
+++ Backend/app.js
@@ -0,0 +1,45 @@
+
+//CARGA DE DEPENDENCIAS
+var express = require('express'); //EXPRESS
+var app = express(); 
+
+var bodyParser = require('body-parser'); //BODY-PARSER 
+
+const cors = require('cors'); //CORS
+const { urlencoded } = require('body-parser');
+
+//IMPLEMENTACION DE DEPENDENCIAS
+
+app.use(cors());
+app.use(express.json);
+app.use(urlencoded());
+
+//Configuracion de CORS
+app.use((req, res, next) =>{
+    res.header("access-controll-allow-origin", "*");
+
+    res.header(
+        "access-control-allow-headers",
+        "authorization, X-API-KEY, origin, X-Requested-With, content-type, accept, Access-Control-Allow-Request-Method"
+    );
+
+    res.header(
+        "Access-Control-Allow-Request-Method",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+
+    res.header("Allow", "GET, POST, PUT, DELETE, OPTIONS");
+
+    next();
+});
+
+//IMPORTACION DE RUTAS
+//
+//
+
+module.exports = app;
+
+
+
+
+
--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,10 @@
-console.log("Hola");
+//Importacion del archivo app en el directorio raiz
+var app = require('./app');
+
+//Definicion del puerto del servidor
+const PORT = 4000 ;
+
+//INICIALIZACION DEL SERVIDOR
+app.listen(PORT, () => {
+    console.log(`Servidor UpImage corriendo en el puerto ${PORT}`)
+});
